a4d068595fb990c0ad4a136e091f1db4e568b629
hriesco <hectorriesco@hotmail.com>
2016-01-21 14:49:54 +0100

    Ruedas de colorines

--- Ficha.cpp
+++ Ficha.cpp
@@ -14,6 +14,20 @@ Ficha::Ficha()
 		hijos[i]->mt->escala(new PV3D(0.1, 0.1, 0.1, 1));
 		hijos[i]->mt->traslada(new PV3D(0.05f*cos(i*inc), 0, 0.05f*sin(i*inc), 1));
 		hijos[i]->mt->rota(i*60, 0, 1, 0);
-		hijos[i]->color->setColor(0, 0, 0);
+
+		if (i == 0)
+			hijos[i]->color->setColor(0, 0, 0);
+		else if (i == 1)
+			hijos[i]->color->setColor(1, 0, 0);
+		else if (i == 2)
+			hijos[i]->color->setColor(0, 1, 0);
+		else if (i == 3)
+			hijos[i]->color->setColor(0, 0, 1);
+		else if (i == 4)
+			hijos[i]->color->setColor(1, 0, 1);
+		else if (i == 5)
+			hijos[i]->color->setColor(0, 1, 1);
+		else if (i == 6)
+			hijos[i]->color->setColor(1, 1, 0);
 	}
 }
