a5b8d841af37626159e0167dc4a0fe095a6fa6fe
hriesco <hectorriesco@hotmail.com>
2016-01-24 19:30:42 +0100

    Añadida la libreria libsoil, agregada desde aptitude libsoil-dev

--- main.cpp
+++ main.cpp
@@ -21,8 +21,30 @@ Camara* cam;
 
 void zoom (GLdouble f);
 
+void applyTexture() {
+	GLuint texturas[2];
+    glGenTextures(2,texturas);
+ 
+    glEnable(GL_TEXTURE_2D);
+ 
+    //Leer archivo
+    int width = 1024;
+    int height = 768;
+    unsigned char* image = SOIL_load_image("carbon.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+ 
+    glBindTexture(GL_TEXTURE_2D, texturas[0]);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    glTexImage2D(GL_TEXTURE_2D, 2, GL_RGB, width,
+	height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+}
+
 void buildSceneObjects() {
 	cubo = new Cubo();
+	applyTexture();
 	cubo->color->setColor(1, 0, 0);
 
 	//Cámara
